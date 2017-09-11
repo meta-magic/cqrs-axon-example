6e95a78485ee0e987d4ae0226d38a7f08e7e0f4b
ashujadhav123 <ashutosh.jadhav@metamagic.in>
2017-09-11 12:35:28 +0530

    commented code removed.

--- ecommapp/eCommApp-shoppingCart/src/main/java/com/ecomm/app/store/ItemInventory.java
+++ ecommapp/eCommApp-shoppingCart/src/main/java/com/ecomm/app/store/ItemInventory.java
@@ -11,24 +11,6 @@ import com.ecomm.app.inventory.repository.ItemRepository;
 @Component
 public class ItemInventory {
 
-	//private Set<Items> items = new HashSet<Items>();
-	
-	/*private static ItemInventory itemStore = new ItemInventory();
-	
-	public static ItemInventory getInstance(){
-		if(itemStore==null){
-			itemStore = new ItemInventory();
-		}
-		return itemStore;
-	}*/
-	
-	/*private ItemInventory(){
-		items.add(new Items("1", "Watch", 10));
-		items.add(new Items("2", "shirt", 12));
-		items.add(new Items("3", "shoes", 8));
-		items.add(new Items("4", "t-shirt", 20));
-	}*/
-
 	@Autowired
 	private ItemRepository itemRepository;
 	
